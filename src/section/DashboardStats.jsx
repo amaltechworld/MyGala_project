e63b6bc4e779d58d7db512a6c7fada5eb5fd5c18
Amal <amalrajsr19@gmail.com>
2025-02-20 16:30:50 +0530

    dashboard-finish

--- src/section/DashboardStats.jsx
+++ src/section/DashboardStats.jsx
@@ -1,49 +1,49 @@
 
 import { DashImg1, DashImg2 } from "../assets/images";
-import {Monitor, Download, Store, ArrowRight } from "lucide-react"; //Monitor, Download
+import {Monitor, Download, Store, ArrowRight } from "lucide-react"; 
 // import { BrowserIcon, MobileDownloadIcon } from "../assets/svg/index.jsx";
 
 const DashboardStats = () => {
     return (
-        <section className="bg-white lg:h-screen flex items-center justify-center">
+        <section className="bg-white lg:h-screen flex items-center justify-center ">
             <div className="container mx-auto p-6">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-0 rounded-2xl overflow-hidden  lg:min-h-[800px] ">
                     {/* Stats Section */}
-                    <div className="bg-[#fff8ba] p-8 lg:p-16 flex flex-col justify-between items-center space-y-8">
+                    <div className="flex flex-row lg:flex-col bg-transparent lg:bg-[#fff8ba] p-0 lg:p-16 justify-between items-stretch space-y-0 lg:space-y-8 order-3 lg:order-1 ">
                         {/* Site Visits */}
-                        <div className="flex items-center gap-4 text-black">
-                            <Monitor className="w-8 h-8 " />
+                        <div className="flex-1 bg-[#fff8ba] lg:bg-transparent flex flex-col lg:flex-row items-center justify-center gap-4 text-black p-4 lg:p-0">
+                            <Monitor className="w-6 h-6 " />
                             <div>
-                                <span className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                                <span className="text-xl md:text-4xl lg:text-6xl font-bold">
                                     200 +
                                 </span>
-                                <p className="text-lg md:text-xl lg:text-2xl font-medium">
+                                <p className="text-sm md:text-lg lg:text-2xl font-medium">
                                     Site Visits
                                 </p>
                             </div>
                         </div>
 
                         {/* Curated Vendors */}
-                        <div className="flex items-center gap-4 text-black">
-                            <Store className="w-8 h-8 " />
+                        <div className="flex-1 bg-[#e5ebff] lg:bg-transparent flex flex-col lg:flex-row items-center justify-center gap-4 text-black p-4 lg:p-0">
+                            <Store className="w-6 h-6 " />
                             <div>
-                                <span className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                                <span className="text-xl md:text-4xl lg:text-6xl font-bold">
                                     165 +
                                 </span>
-                                <p className="text-lg md:text-xl lg:text-2xl font-medium">
+                                <p className="text-sm md:text-lg lg:text-2xl font-medium">
                                     Curated Vendors
                                 </p>
                             </div>
                         </div>
 
                         {/* App Downloads */}
-                        <div className="flex items-center gap-4 text-black">
-                            <Download className="w-8 h-8 " />
+                        <div className="flex-1 bg-[#ffefea] lg:bg-transparent flex flex-col lg:flex-row items-center justify-center gap-4 text-black p-4 lg:p-0">
+                            <Download className="w-6 h-6 " />
                             <div>
-                                <span className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                                <span className="text-xl md:text-5xl lg:text-6xl font-bold">
                                     100 +
                                 </span>
-                                <p className="text-lg md:text-xl lg:text-2xl font-medium">
+                                <p className="text-sm md:text-lg lg:text-2xl font-medium">
                                     App Downloads
                                 </p>
                             </div>
@@ -51,7 +51,7 @@ const DashboardStats = () => {
                     </div>
 
                     {/* Recently Signed-Up Vendor */}
-                    <div className="bg-[#e5ebff] p-8 flex flex-col items-center">
+                    <div className="bg-[#e5ebff] p-8 flex flex-col items-center order-1 lg:order-2">
                         <h2 className="text-2xl font-semibold p-4 lg:p-6 my-4 lg:my-6 text-black">
                             Recently Signed-Up Vendor
                         </h2>
@@ -63,8 +63,12 @@ const DashboardStats = () => {
                         <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2 lg:mb-3 mt-4 lg:mt-6 text-black">
                             Neeraj
                         </h3>
-                        <p className="text-lg md:text-xl lg:text-2xl lg:mt-3 font-semibold text-black leading-relaxed">Photography</p>
-                        <p className="text-gray-600 text-lg md:text-xl lg:text-2xl font-semibold leading-relaxed mb-4 lg:mb-6">Candid Photography</p>
+                        <p className="text-lg md:text-xl lg:text-2xl lg:mt-3 font-semibold text-black leading-relaxed">
+                            Photography
+                        </p>
+                        <p className="text-gray-600 text-lg md:text-xl lg:text-2xl font-semibold leading-relaxed mb-4 lg:mb-6">
+                            Candid Photography
+                        </p>
                         <button className="flex items-center justify-center gap-2 bg-[#11235a] text-white px-6 py-3 w-48 lg:w-52 rounded-lg  hover:bg-blue-900 transition">
                             View Profile
                             <ArrowRight className="w-4 h-4" />
@@ -72,8 +76,8 @@ const DashboardStats = () => {
                     </div>
 
                     {/* Recently Created Invite */}
-                    <div className="bg-[#ffefea] p-8 flex flex-col items-center text-black">
-                        <h2 className="text-2xl font-semibold p-4 lg:p-6 my-4 lg:my-6"> 
+                    <div className="bg-[#ffefea] p-8 flex flex-col items-center text-black order-2 lg:order-3">
+                        <h2 className="text-2xl font-semibold p-4 lg:p-6 my-4 lg:my-6">
                             Recently Created Invite
                         </h2>
                         <img
@@ -92,3 +96,4 @@ const DashboardStats = () => {
 };
 
 export default DashboardStats;
+
