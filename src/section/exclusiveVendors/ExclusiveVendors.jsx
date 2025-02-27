dbf3defe468e80044e90790c5840cd20e3a50060
Amal <amalrajsr19@gmail.com>
2025-02-27 17:29:53 +0530

    Exclusive vendor -desktop version finish

--- src/section/exclusiveVendors/ExclusiveVendors.jsx
+++ src/section/exclusiveVendors/ExclusiveVendors.jsx
@@ -13,7 +13,7 @@ const ExclusiveVendors = () => {
     };
 
     return (
-        <div className="relative bg-[#ffefea] h-screen flex items-center px-10">
+        <div className="relative bg-[#ffefea] h-screen flex items-center px-10 ">
             {/* Left Content - Fixed Column */}
             <div className="w-1/3 space-y-4 z-10">
                 <h1 className="text-5xl text-black font-bold leading-tight font-playfair pb-10">
@@ -24,7 +24,8 @@ const ExclusiveVendors = () => {
                     Explore a range of tailored solutions that elevate your
                     event & celebration to new heights.
                 </p>
-                <button className="bg-[#11235a] text-white px-10 py-4 rounded-md font-poppins">
+                {/* button */}
+                <button className="bg-[#11235a] text-white px-10 py-4 rounded-lg font-poppins">
                     Explore More
                 </button>
             </div>
@@ -33,33 +34,49 @@ const ExclusiveVendors = () => {
             <div className="absolute left-1/3 w-[66vw] flex overflow-hidden h-3/4">
                 <div
                     className="flex transition-transform duration-500 ease-out"
-                    style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+                    style={{ transform: `translateX(-${currentIndex * 25}%)` }}
                 >
-                    {vendorData.map((item) => (
+                    
+                    {vendorData.map((item, index) => (
                         <div
                             key={item.id}
-                            className="relative w-[400px] h-[550px] flex-shrink-0 transition-all duration-500 ease-in-out"
-                        
+                            className={`relative w-[400px] h-[550px] flex-shrink-0 transition-transform duration-500 ease-in-out ${
+                                index === currentIndex
+                                    ? "opacity-100 scale-100 cursor-pointer"
+                                    : "opacity-90 scale-90"
+                            }`}
+                            style={{
+                                transform: `translateX(${
+                                    (index - currentIndex) * 5
+                                }%)`,
+                            }}
+                            onClick={() => setCurrentIndex(index)}
                         >
                             <div className="px-4">
-                                <div className="relative overflow-hidden rounded-lg shadow-md h-full ">
+                                <div
+                                    className={`relative overflow-hidden rounded-lg shadow-md transition-height duration-500 ease-in-out ${
+                                        index === currentIndex
+                                            ? "w-[400px] h-[550px]"
+                                            : "w-[350px] h-[400px]"
+                                    }`}
+                                >
                                     <img
                                         src={item.image}
                                         alt={item.title}
                                         className="w-full h-full object-cover"
                                     />
 
-                                    <div className="absolute bottom-6 left-6 bg-white p-4 rounded-md flex items-center w-[40%] h- pr-12 ">
-                                        <span className="text-lg font-semibold font-poppins">
+                                    <div className="absolute bottom-10 left-6 bg-white bg-opacity-70 p-4  flex items-center w-[60%] py-10 pr-12 ">
+                                        <span className="text-gray-700 text-2xl font-semibold font-poppins">
                                             {item.title}
                                         </span>
                                     </div>
                                     {/* Arrow placed outside */}
-                                    <div className="absolute bottom-6 left-[calc(6%+40%)]  -translate-y-1/2">
+                                    <div className="absolute bottom-6 left-[calc(6%+60%)]  -translate-y-1/2">
                                         <img
                                             src={E_VendorNavigation}
                                             alt="navigation right arrow"
-                                            className="h-auto w-8"
+                                            className="h-auto w-10"
                                         />
                                     </div>
                                 </div>
@@ -78,15 +95,13 @@ const ExclusiveVendors = () => {
             </div>
 
             {/* Bottom Indicators */}
-            <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex gap-2">
+            <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex gap-5 ">
                 {vendorData.map((_, index) => (
                     <div
                         key={index}
                         onClick={() => setCurrentIndex(index)}
                         className={`h-2 w-2 rounded-full cursor-pointer ${
-                            index === currentIndex
-                                ? "bg-navy-800"
-                                : "bg-gray-900"
+                            index === currentIndex ? "bg-black" : "bg-gray-400"
                         }`}
                     />
                 ))}
